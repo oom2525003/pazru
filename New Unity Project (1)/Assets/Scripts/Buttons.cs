eaac7d8655d0d5388a0e6fed84f0075b75b127ce
oom2525052 <oom2525052@stu.o-hara.ac.jp>
2025-11-05 15:23:57 +0900

    start scene

---
+++ New Unity Project (1)/Assets/Scripts/Buttons.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Buttons : MonoBehaviour
+{
+    
+
+    
+
+
+
+    public void switchScene()
+    {
+        SceneManager.LoadScene("GameScene",LoadSceneMode.Single);
+    }
+
+
+}
+
