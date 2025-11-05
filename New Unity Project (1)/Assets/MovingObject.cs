6de384ee96a43074264dc7a42937527723e04314
oom2525003 <oom2525003@stu.o-hara.ac.jp>
2025-11-05 12:15:13 +0900

    object

---
+++ New Unity Project (1)/Assets/MovingObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ìÆÇ≠è·äQï®
+
+public class MovingObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        transform.Translate(-3.0f, 0, 0);
+    }
+}
