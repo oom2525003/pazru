38a869dbe68b662c4aaaec6ba409ec05d43e81f9
oom2525003 <oom2525003@stu.o-hara.ac.jp>
2025-11-19 13:24:32 +0900

    ゴール

--- New Unity Project (1)/Assets/Scripts/Goal.cs
+++ New Unity Project (1)/Assets/Scripts/Goal.cs
@@ -1,25 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag=="Player")
+        // プレイヤーがゴール
+        if (collision.gameObject.tag == "Player")
         {
             Debug.Log("ゴール");
+
+            // コルーチンで停止
+            StartCoroutine(Restart());
         }
     }
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Restart()
     {
-        
+        // ○秒停止する
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(3.0f);
+
+        // 再開する
+        Time.timeScale = 1;
+
+        // クリア画面に移動
+        SceneManager.LoadScene("ClearScene");
     }
 }
+
