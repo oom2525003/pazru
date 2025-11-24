db123d859edbca39e7cd4ddafa864f1cea446d27
oom2525003 <oom2525003@stu.o-hara.ac.jp>
2025-11-25 00:47:52 +0900

    仮マップ作成1

--- New Unity Project (1)/Assets/Scripts/CameraManager.cs
+++ New Unity Project (1)/Assets/Scripts/CameraManager.cs
@@ -1,45 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField] GameObject virtualCamera;
 
-    public GameObject player;
-
-    // 前フレームでのプレイヤーの座標
-    Vector3 prePlayerPos;
-
-
-    // カメラの範囲
-    private const float cameraWidth = 2.5f, cameraHeight = 2.0f;
-
-    private float edgeRight, edgeLeft, edgeUp, edgeDown;
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-
+        if(other.CompareTag("Player")&&!other.isTrigger)
+        {
+            // エリア内はカメラ有効
+            virtualCamera.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D other)
     {
-
-        edgeLeft = transform.position.x + cameraWidth / 2;
-        edgeRight = transform.position.x - cameraWidth / 2;
-        edgeDown = transform.position.y - cameraWidth;
-        edgeUp = transform.position.y + cameraWidth;
-
-        if (player.transform.position != prePlayerPos)
+        // エリア外はカメラ無効
+        if (other.CompareTag("Player") && !other.isTrigger)
         {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
-            prePlayerPos = player.transform.position;
+            virtualCamera.SetActive(false);
         }
-
-        //if (player.transform.position.x>edgeLeft&& player.transform.position.x < edgeRight)
-        //{
-        //    transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0)d;
-        //}
-
     }
 }
--- New Unity Project (1)/Assets/Scripts/PlayerController.cs
+++ New Unity Project (1)/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     // グリッドの設定
-    private const int GRID_WIDTH = 6;
+    private const int GRID_WIDTH = 90;
     private const int GRID_HEIGHT = 4;
 
     // マップのサイズと開始位置
@@ -110,6 +110,8 @@ public class PlayerController : MonoBehaviour
         UpdatePlayerPosition();
 
         Debug.Log($"Playerが移動しました: グリッド({currentX}, {currentY})");
+
+
     }
 
     // プレイヤーの実際の位置を更新
